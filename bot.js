12c29821b3a29591658b45e0bca01c617ac9f61b
maracrea3x <woodadam232@gmail.com>
2018-06-12 12:31:53 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('https://discordapp.com/api/guilds/446488093890052099/widget.json');
+const client = new Discord.Client(https://discordapp.com/api/guilds/456117918888099852/widget.json);
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
