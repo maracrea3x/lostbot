4a10da596ae90d2c631a570f26380cf0e0213e71
maracrea3x <woodadam232@gmail.com>
2018-06-12 12:55:36 -0500

    Update and rename bot.js to lost.js

--- lost.js
+++ lost.js
@@ -1,5 +1,5 @@
 const Discord = require('https://discordapp.com/api/guilds/446488093890052099/widget.json');
-const client = new Discord.Client(https://discordapp.com/api/guilds/456117918888099852/widget.json);
+const client = new Discord.Client('https://discordapp.com/api/guilds/456117918888099852/widget.json');
 
 client.on('ready', () => {
     console.log('I am ready!');
